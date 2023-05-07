f4c62a5d4461257dd935ee0a4b97e742bfaa65e5
jacksonhanks <jacksonjohnrayhanks@gmail.com>
2023-05-07 17:14:36 -0400

    added quiz questions to JS

---
+++ Assets/script.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        question: "JavaScript primarily handles what?",
+        options: [
+            "Writing webpage content",
+            "Formating webpage content",
+            "Overall functionality of an application",
+            "Managing repositories",
+        ],
+        answer: "Formating webpage content",
+    },
+    {
+        question: "Which of the following displays best practice for naming a variable in JavaScript?",
+        options: [
+            "var quizQuestions",
+            "var Quizquestions",
+            "var quiz questions",
+            "var quizquestions",
+        ],
+        answer: "var quizQuestions",
+    },
+    {
+        question: "Which of the following keywords CANNOT be used to define a variable in JavaScript?",
+        options: [
+            "var",
+            "div",
+            "const",
+            "let",
+        ],
+        answer: "div",
+    },
+    {
+        question: "How would one call an element with ID from the HTML in JavaScript?",
+        options: [
+            "document.id()",
+            "getElementById()",
+            "document.getElementById()",
+            "document.getelementbyid()",
+        ],
+        answer: "document.getElementById()",
+    },
+    {
+        question: "How does one add a comment in JavaScript?",
+        options: [
+            "--comment--",
+            "<!--comment-->",
+            "/*comment*/",
+            "//comment",
+        ],
+        answer: "//comment",
+    },
+];
